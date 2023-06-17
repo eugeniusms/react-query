5cef397c7f5ef6f533206974e000330510b50215
Eugenius Mario <eugenius.mario@ui.ac.id>
2023-06-18 02:57:35 +0700

    feat: handling query error

--- components/modules/superheroes/SuperHeroes.tsx
+++ components/modules/superheroes/SuperHeroes.tsx
@@ -4,17 +4,29 @@ import { useEffect, useState } from 'react';
 export const SuperHeroes = () => {
   const [isLoading, setIsLoading] = useState(true);
   const [data, setData] = useState([]);
+  const [error, setError] = useState('');
 
   useEffect(() => {
-    axios.get('http://localhost:8000/superheroes').then((res) => {
-      setData(res.data);
-      setIsLoading(false);
-    });
+    axios
+      .get('http://localhost:8000/superheroes')
+      .then((res) => {
+        setData(res.data);
+        setIsLoading(false);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsLoading(false);
+      });
   }, []);
 
   if (isLoading) {
     return <div>Loading...</div>;
   }
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+    
   return (
     <>
       <h2>Super Heroes Page</h2>
--- components/modules/superheroes/SuperHeroesReactQuery.tsx
+++ components/modules/superheroes/SuperHeroesReactQuery.tsx
@@ -1,20 +1,34 @@
 import axios from 'axios';
 import { useQuery } from 'react-query';
 
+interface Superhero {
+  id: number;
+  name: string;
+}
+
 const fetchSuperHeroes = async () => {
   const { data } = await axios.get('http://localhost:8000/superheroes');
   return data;
 };
 
 export const SuperHeroesReactQuery = () => {
-  const { isLoading, data } = useQuery('superheroes', fetchSuperHeroes);
+  const { isLoading, data, isError, error } = useQuery(
+    'superheroes',
+    fetchSuperHeroes
+  );
+
   if (isLoading) {
     return <div>Loading...</div>;
   }
+
+  if (isError) {
+    return <div>{error.message}</div>;
+  }
+
   return (
     <>
       <h2>Super Heroes React Query Page</h2>
-      {data?.map((hero: any) => (
+      {data?.map((hero: Superhero) => (
         <div key={hero.id}>{hero.name}</div>
       ))}
     </>
